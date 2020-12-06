da66a018e3491236c4cbbaa99e522c06434017db
albane tonnellier <albanetonnellier@gmail.com>
2020-12-06 17:21:20 +0000

    landing

---
+++ app/javascript/channels/landing.js
@@ -0,0 +1,11 @@
+$('.base').clone().addClass('overlay').appendTo('.landing');
+$('.cta').hover(function() {
+  $('.overlay').toggleClass('over');
+});
+
+
+setTimeout(function() {
+  $('.overlay').addClass('over').delay(600).queue(function() {
+    $(this).removeClass("over").dequeue();
+  });
+}, 400)
